c3eea5258117c017749053a5fa15c2ba0c8cab1a
Kai Fukushima <k_fukushima@yumemi.co.jp>
2021-06-13 23:22:04 +0900

    fix: init method

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -3,6 +3,7 @@ import { Command } from "commander";
 import prompts from "prompts";
 
 import { createInits, loadCredentials, saveCredentails } from "../file-systems";
+import { skipLine } from "../utils";
 
 export const makeInitCommand = () => {
   const program = new Command("init");
@@ -13,17 +14,21 @@ export const makeInitCommand = () => {
     // load credentials
     const credentials = await loadCredentials();
 
+    skipLine();
+
+    // ask notion token
     const notion_token = await askNotionToken();
 
     if (!notion_token) {
-      console.warn("no input found");
+      console.warn(chalk.yellow.bold("no inputs were found"));
     } else {
       credentials["NOTION_TOKEN"] = notion_token;
       await saveCredentails(credentials);
+      skipLine();
+      console.log(`use ${chalk.green.bold("'c2n db --new'")} to add databases`);
     }
-    console.log(
-      chalk.green.bold("use ") + chalk.blueBright.bold("c2n db --new") + chalk.green.bold(" to add databases"),
-    );
+
+    skipLine();
   });
 
   return program;
@@ -33,7 +38,7 @@ const askNotionToken = async () => {
   const res = await prompts({
     type: "invisible",
     name: "value",
-    message: "input slack token",
+    message: chalk.green.bold("input notion token"),
   });
   return res.value;
 };
